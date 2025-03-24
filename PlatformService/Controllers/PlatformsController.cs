8bea38aa22138f89b18026609c2ec6e79e08c0ef
Nikos Vourdoumpas <nvourdoumpas@hotmail.com>
2025-03-25 00:44:54 +0200

    feat: implement controller and actions for platforms

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _platformRepo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _platformRepo = platformRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms...");
+            var platformItems = _platformRepo.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _platformRepo.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _platformRepo.CreatePlatform(platformModel);
+            _platformRepo.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+        }
+    }
+}
