3a5f34483ab2e1a979d04992d6145cd212478177
Nikos Vourdoumpas <nvourdoumpas@hotmail.com>
2025-03-24 21:11:58 +0200

    feat: add di for platform repo and add gitignore

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -28,6 +28,9 @@ namespace PlatformService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
--- PlatformService/obj/Debug/net5.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net5.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb1c5760d0cd7a0551a8359596e9ac5c7b0e7b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65da4b698a2a037ec56c81410e76a5cb82d4f31")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
