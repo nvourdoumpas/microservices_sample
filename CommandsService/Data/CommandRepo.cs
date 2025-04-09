aa4cf286c2f0bb110e47de0727341d458250da28
Nikos Vourdoumpas <nvourdoumpas@hotmail.com>
2025-04-10 00:34:33 +0300

    feat: implement event processor at command service

--- CommandsService/Data/CommandRepo.cs
+++ CommandsService/Data/CommandRepo.cs
@@ -35,6 +35,11 @@ namespace CommandsService.Data
             _context.Platforms.Add(plat);
         }
 
+        public bool ExternalPlatformExist(int externalPlatformId)
+        {
+            return _context.Platforms.Any(p => p.ExternalID == externalPlatformId);
+        }
+
         public IEnumerable<Platform> GetAllPlatforms()
         {
             return _context.Platforms;
